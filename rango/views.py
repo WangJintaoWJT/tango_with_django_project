8e045dbc2847c6e66457666a2d56bdec223f5005
Jintao Wang <2359479w@student.gla.ac.uk>
2019-02-11 12:47:38 +0000

    New Update

---
+++ rango/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rango.models import Category
+from rango.models import Page
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'category': category_list}
+    return render(request, 'rango/index.html', context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['page'] = pages
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
+def about(request):
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html',{})
